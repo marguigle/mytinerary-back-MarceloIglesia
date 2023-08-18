131a0a527aca070053646a3d1f3123ae71e92ab2
Marcelo G Iglesia <marguigle@yahoo.com.ar>
2023-08-17 21:47:50 -0300

    se logra cargar la base de datos

--- models/CityModel.js
+++ models/CityModel.js
@@ -1,11 +1,17 @@
-import mongoose from "mongoose";
 import { Schema, model, Types } from "mongoose";
 
-let collection = "cities";
-let schema = new schema({
-  image: { type: String, required: true },
-  name: { type: String, required: true },
-  lenguage: { type: String, required: true },
+const schema = new Schema(
+  {
+    image: { type: String, required: true },
+    name: { type: String, required: true },
+    language: { type: String, required: true }, // Corregido "lenguage" a "language"
+    description: { type: String, required: true },
+    created_by: { type: Types.ObjectId, ref: "User" }, // Reemplazado "" con el nombre de modelo correcto
+  },
+  {
+    timestamps: true,
+  }
+);
 
-  description: { type: String, required: true },
-});
+const CitiesModel = model("City", schema); // Cambiado "collection" por "City"
+export default CitiesModel; // Cambiado "Category" por "Cities"
