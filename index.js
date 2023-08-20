7bca4f2ab91ccad4e726e4de11d92b64d4388b14
Marcelo G Iglesia <marguigle@yahoo.com.ar>
2023-08-20 13:59:17 -0300

    primer commit de prueba1

--- index.js
+++ index.js
@@ -1,16 +1,14 @@
-import "dotenv/config.js";
-import "./config/database.js";
 import express from "express";
 import indexRouter from "./router/indexRouter.js";
-import cors from "cors";
-const server = express();
-server.use("/", indexRouter);
-server.use(cors());
-server.use(express.json());
 
-server.listen(process.env.PORT, () => {
-  console.log("server ready on port " + process.env.PORT);
+const server = express();
+const PORT = 3000;
+server.listen(PORT, () => {
+  console.log("server on port " + PORT);
 });
+
 server.get("/", (req, res, next) => {
-  res.send(console.log("bienvenido a mi servidor en /"));
+  res.send("bienvenido a mi servidor en /");
 });
+
+server.use("/api", indexRouter);
--- router/indexRouter.js
+++ router/indexRouter.js
@@ -1,16 +1,12 @@
 import { Router } from "express";
-import citiesController from "../controller/citiesController.js";
 
-const { getAllCities, getOneCity, updateOneCity, deleteOneCity } =
-  citiesController;
 const indexRouter = Router();
 
-indexRouter.get("/api", (request, response, next) => {
-  response.send("Bienvenido a mi servidor en /api");
+indexRouter.get("/", (req, res, next) => {
+  res.send("bienvenido a mi servidor en /api");
 });
-indexRouter.get("/cities", getAllCities);
-indexRouter.get("/cities/:id", getOneCity);
-indexRouter.get("/cities/:id", updateOneCity);
-indexRouter.get("/cities/:id", deleteOneCity);
+indexRouter.get("/cities", (req, res, next) => {
+  res.send("Bienvenido a mi servidor en /cities");
+});
 
 export default indexRouter;
