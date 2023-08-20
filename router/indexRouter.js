2fd7044ab2168124cf80a67c48f4f2aae26a7222
Marcelo G Iglesia <marguigle@yahoo.com.ar>
2023-08-20 14:05:29 -0300

    se agrega cityArray harcodeado

--- router/indexRouter.js
+++ router/indexRouter.js
@@ -1,12 +1,17 @@
 import { Router } from "express";
-
+import cityArray from "../cityArray.js";
+cityArray;
 const indexRouter = Router();
 
 indexRouter.get("/", (req, res, next) => {
   res.send("bienvenido a mi servidor en /api");
 });
 indexRouter.get("/cities", (req, res, next) => {
-  res.send("Bienvenido a mi servidor en /cities");
+  res.json({
+    response: cityArray,
+    succes: true,
+    error: null,
+  });
 });
 
 export default indexRouter;
