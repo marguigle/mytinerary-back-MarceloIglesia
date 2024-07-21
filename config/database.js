55dd500e57ad7208463e25ec3b3312e23a9110c1
Marcelo G Iglesia <marguigle@yahoo.com.ar>
2024-07-21 17:09:59 -0300

    nueva correccion de base de datos

--- config/database.js
+++ config/database.js
@@ -1,17 +1,9 @@
-// import mongoose from "mongoose";
-
-// mongoose
-//   .connect("mongodb+srv://marguigle:basededatos@cluster0.orvohzd.mongodb.net/")
-//   .then(() => console.log("Database connected"))
-//   .catch(() => console.log("Database connection failed"));
-
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 
-// Cargar variables de entorno desde un archivo .env
 dotenv.config();
 
-const dbURL = process.env.DATABASE_URL;
+const dbURL = process.env.DB_URL;
 
 mongoose
   .connect(dbURL)
